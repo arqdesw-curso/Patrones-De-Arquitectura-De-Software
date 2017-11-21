db2c58df3fe88b7b9852cb1bb71366beca5bf936
Alexander Padilla <yamop.16@gmail.com>
2017-11-21 16:12:37 +0000

    Update api

--- c05/micro_servicios/sv_information.py
+++ c05/micro_servicios/sv_information.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-#!/usr/bin/env python
-#----------------------------------------------------------------------------------------------------------------
+# !/usr/bin/env python
+# ----------------------------------------------------------------------------------------------------------------
 # Archivo: information_mc.py
 # Capitulo: 5 Estilo Microservicios.
 # Autor(es): Perla Velasco & Yonathan Mtz.
@@ -29,26 +29,35 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import urllib, json
-app = Flask (__name__)
+
+app = Flask(__name__)
+
 
 @app.route("/api/v1/information")
 def get_information():
-	# Método que obtiene la información de OMDB acerca de un título en particular
-	# Se obtiene el parámetro 't' que contiene el título de la película o serie que se va a consultar
-	title = request.args.get("t")
-	# Se conecta con el servicio de OMDB a través de su API que OMDB ofrece
-	url_omdb = urllib.urlopen("http://www.omdbapi.com/?t="+title+"&plot=full&r=json")
-	# Se lee la respuesta de OMDB
-	json_omdb = url_omdb.read()
-	# Se convierte en un JSON la respuesta recibida
-	omdb = json.loads(json_omdb)
-	# Se regresa como respuesta el JSON que se recibió del API de OMDB
-	return json.dumps(omdb)
+    """
+        Este método obtiene información acerca de una película o serie
+        específica.
+        :return: JSON con la información de la película o serie
+        """
+    # Se lee el parámetro 't' que contiene el título de la película o serie que se va a consultar
+    title = request.args.get("t")
+    api_key = '7aed8d98'
+    url_base = 'http://www.omdbapi.com/?apikey=' + api_key + '&t='
+    # Se conecta con el servicio de IMDb a través de su API
+    url_omdb = urllib.urlopen(url_base + title + "&plot=full&r=json")
+    # Se lee la respuesta de IMDb
+    json_omdb = url_omdb.read()
+    # Se convierte en un JSON la respuesta recibida
+    omdb = json.loads(json_omdb)
+    # Se regresa el JSON de la respuesta
+    return json.dumps(omdb), 200
+
 
 if __name__ == '__main__':
-	# Se define el puerto del sistema operativo que utilizará el micro servicio
-	port = int(os.environ.get('PORT', 8084))
-	# Se habilita la opción de 'debug' para visualizar los errores
-	app.debug = True
-	# Se ejecuta el micro servicio definiendo el host '0.0.0.0' para que se pueda acceder desde cualquier IP
-	app.run(host='0.0.0.0', port=port)
+    # Se define el puerto del sistema operativo que utilizará el micro servicio
+    port = int(os.environ.get('PORT', 8084))
+    # Se habilita la opción de 'debug' para visualizar los errores
+    app.debug = True
+    # Se ejecuta el micro servicio definiendo el host '0.0.0.0' para que se pueda acceder desde cualquier IP
+    app.run(host='0.0.0.0', port=port)
